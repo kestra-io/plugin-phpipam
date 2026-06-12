6a7e742659ab3c17e218b5e5926ad38d540dbb5e
jymaire <jmaire@kestra.io>
2026-06-12 08:51:36 +0200

    chore: initial scaffold cleanup for plugin-phpipam

---
+++ src/main/java/io/kestra/plugin/phpipam/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "PhpIPAM",
+    description = "PhpIPAM plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.phpipam;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
